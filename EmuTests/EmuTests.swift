40158ec54e19e4f45a4634fd67688ba0d4a9dd80
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2025-12-02 20:55:30 +0700

    Initial Commit

---
+++ EmuTests/EmuTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmuTests.swift
+//  EmuTests
+//
+//  Created by Azzam Ubaidillah on 02/12/25.
+//
+
+import Testing
+@testable import Emu
+
+struct EmuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
